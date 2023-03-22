853acede161530bde946e933a422be601ef1b60b
Bhagirath <bhagirathkumar813110@gmail.com>
2023-03-22 17:52:09 +0530

    change the file

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const port=8000;
+const app=express();
+app.listen(port,function(err){
+
+    if(err)
+    {
+        console.log(err);
+    }
+    console.log('Server is Running',port);
+
+});
